c97518be12ba53512475d3de6401a16eadfb8fdc
halgfdc <99038343+halgfdc@users.noreply.github.com>
2024-09-07 07:47:02 +0800

    Update xiaomai.js

--- xiaomai.js
+++ xiaomai.js
@@ -1,3 +1,14 @@
+/*************************************
+
+项目名称：小麦在线
+下载地址：https://t.cn/A6TG09ZZ
+更新日期：2024-03-27
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
 [rewrite_local]
 ^https?:\/\/www\.xiaomaigongkao\.com\/AppApi\/(vip|StartPage\/welcome|Member\/indexHangingWindow|Course\/(curriculum|details)) url script-response-body https://raw.githubusercontent.com/halgfdc/113/main/xiaomai.js
 
